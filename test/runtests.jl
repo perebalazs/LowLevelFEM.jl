5697c4f0766ca87c186487b4c7366f568f073ead
perebalazs <43467760+perebalazs@users.noreply.github.com>
2024-02-18 23:14:13 +0100

    update versions

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,9 +1,12 @@
-using LowLevelFEM
+include("../src/LowLevelFEM.jl")
+import LowLevelFEM as FEM
 using Test
 
 @testset "LowLevelFEM.jl" begin
     # Write your tests here.
     #@test LowLevelFEM.greet_your_package_name() == "Hello LowLevelFEM!"
     #@test LowLevelFEM.greet_your_package_name() != "Hello world!"
-    @test 1 == 1
+    returntest = "test", 0, 0, 0
+    @test FEM.displacementConstraint("test", ux=0, uy=0, uz=0) == returntest
+    display(FEM.displacementConstraint("test", ux=0, uy=0, uz=0))
 end
