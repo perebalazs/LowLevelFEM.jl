57efa77e5397638e3cf54328b4978b91093656c6
perebalazs <43467760+perebalazs@users.noreply.github.com>
2024-02-18 20:40:13 +0100

    add dependencies

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,4 +5,5 @@ using Test
     # Write your tests here.
     #@test LowLevelFEM.greet_your_package_name() == "Hello LowLevelFEM!"
     #@test LowLevelFEM.greet_your_package_name() != "Hello world!"
+    @test 1 == 1
 end
