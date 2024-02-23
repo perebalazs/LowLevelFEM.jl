bdfd8a09a33f3e414a3df6abc4eb6075de89d44b
perebalazs <43467760+perebalazs@users.noreply.github.com>
2024-02-23 10:00:25 +0100

    update docs

--- docs/make.jl
+++ docs/make.jl
@@ -1,16 +1,16 @@
-push!(LOAD_PATH,"../src/")
-include("../src/LowLevelFEM.jl")
-using Documenter, .LowLevelFEM
+#push!(LOAD_PATH,"../src/")
+#include("../src/LowLevelFEM.jl")
+using Documenter, LowLevelFEM
 
-DocMeta.setdocmeta!(LowLevelFEM, :DocTestSetup, :(using LowLevelFEM); recursive=true)
+#DocMeta.setdocmeta!(LowLevelFEM, :DocTestSetup, :(using LowLevelFEM); recursive=true)
 
 #makedocs(sitename="LowLevelFEM", format=Documenter.LaTeX())
 makedocs(;sitename="LowLevelFEM",
     authors="Balázs Pere",
 #    repo="https://github.com/perebalazs/LowLevelFEM.jl/stable/{commit}{path}#{line}",
     format=Documenter.HTML(;
-        canonical="https://docs.juliahub.com/General/LowLevelFEM/stable/",
-        assets=String[],
+#        canonical="https://docs.juliahub.com/General/LowLevelFEM/stable/",
+#        assets=String[],
     ),
     pages=[
         "Home" => "index.md",
@@ -20,8 +20,8 @@ makedocs(;sitename="LowLevelFEM",
 deploydocs(; repo="github.com/perebalazs/LowLevelFEM.jl.git"#,
     #devbranch = "main",
     #devurl="dev",
-    target = "build",
+#    target = "build",
     #branch = "gh-pages",
     #versions = ["stable" => "v^", "v#.#" ]
-    push_preview = true,
+#    push_preview = true,
 )
