c383bb74f36d1d1791fd3eb4791f2bca426bbe5d
perebalazs <pere.balazs@gmail.com>
2025-08-25 23:04:28 +0200

    Update make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -1,4 +1,7 @@
-push!(LOAD_PATH,"../src/")
+using Pkg
+Pkg.activate(@__DIR__)
+Pkg.develop(PackageSpec(path=joinpath(@__DIR__, "..")))
+push!(LOAD_PATH, joinpath(@__DIR__, "../src"))
 using Documenter
 using LowLevelFEM
 
